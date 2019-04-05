69bdebd64a245bdbfab16b6fe6f0a2787373e034
Praveen Chinta <cvsnpraveen@gmail.com>
2019-04-05 19:59:48 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,29 +2,48 @@ import React, { Component } from 'react';
 import TodoForm from './TodoForm';
 import Nav from './Components/Nav';
 import DataGrid from './Components/DataGrid';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    }
+
+    this.onChange = (e, t) => {
+      const newState = { ...this.state };
+      let { name, value } = e.target;
+
+      if (t === 'check') {
+        let elem = document.getElementById(newState.data[name].number);
+        newState.data[name].choosen = !newState.data[name].choosen;
+
+        { elem && elem.focus() }
+        { elem && elem.select() }
+      }
+      if (t === 'text') {
+        newState.data[name].installment = value;
+      }
+
+      this.setState({
+        data: newState.data
+      })
+    }
+
+  }
+  componentDidMount() {
+    axios.get('./files/data.json')
+      .then((res) => this.setState({ data: res.data }));
+  }
+
   render() {
+    const gridData = this.state.data;
     return (
       <div className="App">
         <Nav></Nav>
-
-        <DataGrid></DataGrid>
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header> */}
+        <DataGrid data={gridData} onChange={this.onChange} ></DataGrid>
       </div>
     );
   }
