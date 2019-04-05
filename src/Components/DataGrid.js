9c9e2bb300af20cf080e3ab83775ac868107bfd7
Praveen Chinta <cvsnpraveen@gmail.com>
2019-04-05 20:01:01 +0800

    Update DataGrid.js

--- src/Components/DataGrid.js
+++ src/Components/DataGrid.js
@@ -1,50 +1,40 @@
 import React from 'react';
-import axios from 'axios';
 
 class DataGrid extends React.Component {
-    constructor(){
-        super();
-        this.state = {
-            data: [],
-            bill: []
-        }
-    }
-    componentDidMount(){
-        axios.get('./files/data.json')
-        .then((res)=>this.setState({data: res.data}));
-    }
     render() {
-        const data = this.state.data;
+        const data = this.props.data;
         return (
-            <table className="table table-bordered">
-                <thead>
-                    <tr className="info">
-                        <th>S.No</th>
-                        <th>&nbsp;</th>
-                        <th>Account Name</th>
-                        <th>Account Number</th>
-                        <th>Denomination</th>
-                        <th>Month Paid Upto</th>
-                        <th>No of Installments</th>
-                        <th>Due Date</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {data&&
-                    data.map((item, i)=>
-                        <tr key={i}>
-                        <td>{i+1}</td>
-                        <td><input type="checkbox" value={item} name="" /></td>
-                        <td>{item.name}</td>
-                        <td>{item.number}</td>
-                        <td>{item.denomination}</td>
-                        <td>{item.paidUpto}</td>
-                        <td>{item.instal}</td>
-                        <td>{item.dueDate}</td>
-                    </tr>
-                    )}
-                </tbody>
-            </table>
+            <form>
+                <table className="table table-bordered">
+                    <thead>
+                        <tr className="info">
+                            <th>S.No</th>
+                            <th>&nbsp;</th>
+                            <th>Account Name</th>
+                            <th>Account Number</th>
+                            <th>Denomination</th>
+                            <th>Month Paid Upto</th>
+                            <th>No of Installments</th>
+                            <th>Due Date</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data &&
+                            data.map((item, i) =>
+                                <tr key={i}>
+                                    <td>{i + 1}</td>
+                                    <td><input type="checkbox" checked={item.choosen} name={i} onChange={(e) => this.props.onChange(e, "check")} /></td>
+                                    <td>{item.name}</td>
+                                    <td>{item.number}</td>
+                                    <td>{item.denomination}</td>
+                                    <td>{item.paidUpto}</td>
+                                    <td><input type="text" value={item.installment} name={i} id={item.number} onChange={(e) => this.props.onChange(e, "text")} /></td>
+                                    <td>{item.dueDate}</td>
+                                </tr>
+                            )}
+                    </tbody>
+                </table>
+            </form>
         )
     }
 }
